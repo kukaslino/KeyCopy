dac9bc6974f09e4348228bd6a198528b9231ada6
kukaslino <cauegirardi@gmail.com>
2024-11-25 19:11:01 -0300

    tst

--- lib/Logic/logic.h
+++ lib/Logic/logic.h
@@ -1,7 +1,12 @@
 #ifndef LOGIC_H
 #define LOGIC_H
 
+#ifdef UNIT_TEST
+#include <iostream> // Para simular entradas e saídas no ambiente nativo
+#include <string>
+#else
 #include <Arduino.h>
+#endif
 
 String encrypt(String data, String key);
 
